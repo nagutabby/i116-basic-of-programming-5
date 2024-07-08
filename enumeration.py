9aab198a65a3206e98a3d1d389f7fe4f7e6428fd
nagutabby <62084485+nagutabby@users.noreply.github.com>
2024-07-08 20:51:12 +0900

    インデントを修正する

--- enumeration.py
+++ enumeration.py
@@ -8,19 +8,19 @@ class Thing(Enum):
     Poison: auto = auto()
     Nothing: auto = auto()
 
-def __str__(self) -> str:
-    if self == Thing.Gold:
-        return 'Gold'
-    elif self == Thing.Silver:
-        return 'Silver'
-    elif self == Thing.Stone:
-        return 'Stone'
-    elif self == Thing.Poison:
-        return 'Poison'
-    elif self == Thing.Nothing:
-        return 'Nothing'
-    else:
-        return 'Error'
+    def __str__(self) -> str:
+        if self == Thing.Gold:
+            return 'Gold'
+        elif self == Thing.Silver:
+            return 'Silver'
+        elif self == Thing.Stone:
+            return 'Stone'
+        elif self == Thing.Poison:
+            return 'Poison'
+        elif self == Thing.Nothing:
+            return 'Nothing'
+        else:
+            return 'Error'
 
 print(Thing.Gold)
 print(Thing.Silver)
@@ -28,10 +28,10 @@ print(Thing.Stone)
 print(Thing.Poison)
 print(Thing.Nothing)
 
-n1: Node = Node(str(Thing.Poison), Leaf(), Leaf())
-n2: Node = Node(str(Thing.Stone), Leaf(), Leaf())
-n3: Node = Node(str(Thing.Stone), n1, n2)
-n4: Node = Node(str(Thing.Silver), Leaf(), Leaf())
-n5: Node = Node(str(Thing.Gold), Leaf(), Leaf())
-n6: Node = Node(str(Thing.Stone), n4, n5)
-n8: Node = Node(str(Thing.Silver), n3, n6)
+n1: Node = Node(Thing.Poison, Leaf(), Leaf())
+n2: Node = Node(Thing.Stone, Leaf(), Leaf())
+n3: Node = Node(Thing.Stone, n1, n2)
+n4: Node = Node(Thing.Silver, Leaf(), Leaf())
+n5: Node = Node(Thing.Gold, Leaf(), Leaf())
+n6: Node = Node(Thing.Stone, n4, n5)
+n8: Node = Node(Thing.Silver, n3, n6)
